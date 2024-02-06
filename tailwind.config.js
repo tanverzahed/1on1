11edc38f7f7a2a809bb370f587a424eabe948ccb
tanverzahed <tenberabu@gmail.com>
2024-02-06 14:45:12 -0500

    Finished login, signup, progress on contacts

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html",
+            "./src/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
